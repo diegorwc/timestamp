618672f2ca9fa89db77175d3252a2e2040977cfa
Diego <wille.diegoricardo@gmail.com>
2022-07-01 19:27:45 -0300

    unix timestamp done

--- index.js
+++ index.js
@@ -26,17 +26,24 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/:date/", function(req, res) {
-  const dateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
+  const dateRegex = /^(\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))$/
+  console.log(/[1-9]\d+/.test(req.params.date))
   if(dateRegex.test(req.params.date)) {
     let dateObj = new Date(req.params.date)
     let unixDate = Number(Date.parse(req.params.date))
     let splitDate = req.params.date.split('-')    
-    let utcDate = dateObj.toUTCString()
+    let utcDate = dateObj.toUTCString()            
     res.json({
       'unix': unixDate,
       'utc': utcDate
     })
-  } else {
+  } else if(/[1-9]\d+/.test(req.params.date)) {
+    res.json({
+      'unix': req.params.date,
+      'utc': new Date(Number(req.params.date)).toUTCString()
+    })
+  } 
+  else {
     res.json({'error': 'Invalid Date'})  
   }
   
